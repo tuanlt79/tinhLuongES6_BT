916758b1bdecd9f41b6e662c475c31ea51e7e391
tuanlt79 <63086228+tuanlt79@users.noreply.github.com>
2021-01-03 10:10:39 +0700

    Commit 1

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.getElementById("btnTinhLuong").onclick = (event) => {
+    event.preventDefault();
+    let nhanVien = {};
+    let arrInput = document.querySelectorAll(".form-input input");
+    console.log(arrInput);
+};
