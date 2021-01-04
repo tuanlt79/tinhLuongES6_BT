c6c477e65cab69db9cec47a8e67f4e4b8c9b9036
tuanlt79 <tuanlt79@outlook.com.vn>
2021-01-04 16:11:03 +0700

    C2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,38 @@
 document.getElementById("btnTinhLuong").onclick = (event) => {
     event.preventDefault();
     let nhanVien = {};
+    let arrTd=[];
     let arrInput = document.querySelectorAll(".form-input input");
-    console.log(arrInput);
+    let chucVu = document.getElementById("selectCV").value;
+    
+   //duyệt for trên ary input
+    for (let i = 0; i < arrInput.length; i++) {
+        
+        // mỗi lần duyệt lấy ra 1 tagInput
+        let tagInput = arrInput[i];
+        //lấy ra id, value tag input
+        let { id, value,title }= tagInput;
+        nhanVien = { ...nhanVien, [id]: value };
+            //tạo thead cho tr
+        arrTd = [...arrTd, title];
+       
+
+    };
+    //Tạo cột
+    let contentTr = `<tr>`;
+    for (let i = 0; i <arrTd.length; i++)
+    {   contentTr+=`<td>${arrTd[i]}</td>`
+    }
+    contentTr+=`</tr>`
+    document.querySelector('thead').innerHTML = contentTr;
+    //duyệt đối tượng học viên
+
+
+    let contentTrBody= '<tr>';
+    for (let thuocTinh in nhanVien) {
+        contentTrBody+=`<td>${nhanVien[thuocTinh]}</td>`
+        nhanVien[thuocTinh];
+    }
+    contentTrBody + `</tr>`
+    document.querySelector('tbody').innerHTML = contentTrBody;
 };
