dd4d581f8daf2ad21083a84192f34e9b55fc7018
tuanlt79 <tuanlt79@outlook.com.vn>
2021-01-06 10:36:08 +0700

    C3

--- js/index.js
+++ js/index.js
@@ -2,9 +2,7 @@ document.getElementById("btnTinhLuong").onclick = (event) => {
     event.preventDefault();
     let nhanVien = {};
     let arrTd=[];
-    let arrInput = document.querySelectorAll(".form-input input");
-    let chucVu = document.getElementById("selectCV").value;
-    
+    let arrInput = document.querySelectorAll(".form-input input,select ");
    //duyệt for trên ary input
     for (let i = 0; i < arrInput.length; i++) {
         
@@ -15,24 +13,27 @@ document.getElementById("btnTinhLuong").onclick = (event) => {
         nhanVien = { ...nhanVien, [id]: value };
             //tạo thead cho tr
         arrTd = [...arrTd, title];
-       
-
     };
+    let { selectCV, luongCoBan } = nhanVien;
+    let tongLuong = (Number(selectCV) * Number(luongCoBan));
+    // console.log(tongLuong);
     //Tạo cột
-    let contentTr = `<tr>`;
+    let contentTr = `<tr>`
     for (let i = 0; i <arrTd.length; i++)
-    {   contentTr+=`<td>${arrTd[i]}</td>`
+    {
+        contentTr += `<td class="text-warning">${arrTd[i]}</td> `
     }
-    contentTr+=`</tr>`
+    contentTr+=`<td class="text-warning">Tổng Lương</td>`
+    contentTr += `</tr>`
     document.querySelector('thead').innerHTML = contentTr;
-    //duyệt đối tượng học viên
 
-
+    //duyệt đối tượng nhân viên
     let contentTrBody= '<tr>';
     for (let thuocTinh in nhanVien) {
         contentTrBody+=`<td>${nhanVien[thuocTinh]}</td>`
         nhanVien[thuocTinh];
     }
+    contentTrBody+=`<td>${tongLuong}</td>`
     contentTrBody + `</tr>`
     document.querySelector('tbody').innerHTML = contentTrBody;
 };
