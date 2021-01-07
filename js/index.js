2e8182e8986e3a763a9f502786fbeafd3b7d00e4
tuanlt79 <63086228+tuanlt79@users.noreply.github.com>
2021-01-07 12:34:59 +0700

    Checkout_Branch

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 document.getElementById("btnTinhLuong").onclick = (event) => {
     event.preventDefault();
     let nhanVien = {};
-    let arrTd=[];
+    let arrSpan=[];
     let arrInput = document.querySelectorAll(".form-input input,select ");
    //duyệt for trên ary input
     for (let i = 0; i < arrInput.length; i++) {
@@ -11,29 +11,29 @@ document.getElementById("btnTinhLuong").onclick = (event) => {
         //lấy ra id, value tag input
         let { id, value,title }= tagInput;
         nhanVien = { ...nhanVien, [id]: value };
-            //tạo thead cho tr
-        arrTd = [...arrTd, title];
+            //tạo span 
+        arrSpan = [...arrSpan, title];
+        console.log(arrSpan);
     };
+    
     let { selectCV, luongCoBan } = nhanVien;
     let tongLuong = (Number(selectCV) * Number(luongCoBan));
-    // console.log(tongLuong);
-    //Tạo cột
-    let contentTr = `<tr>`
-    for (let i = 0; i <arrTd.length; i++)
-    {
-        contentTr += `<td class="text-warning">${arrTd[i]}</td> `
-    }
-    contentTr+=`<td class="text-warning">Tổng Lương</td>`
-    contentTr += `</tr>`
-    document.querySelector('thead').innerHTML = contentTr;
 
-    //duyệt đối tượng nhân viên
-    let contentTrBody= '<tr>';
-    for (let thuocTinh in nhanVien) {
-        contentTrBody+=`<td>${nhanVien[thuocTinh]}</td>`
-        nhanVien[thuocTinh];
+    let contentSpan = ``;
+    for (let i = 0; i < arrSpan.length; i++){
+        contentSpan += `<p class="text-warning">${arrSpan[i]} :</p> `;
     }
-    contentTrBody+=`<td>${tongLuong}</td>`
-    contentTrBody + `</tr>`
-    document.querySelector('tbody').innerHTML = contentTrBody;
+    contentSpan += `<p class="text-warning">Tổng lương: ${tongLuong}</p>`;
+    
+    document.querySelector(".container .bg-dark form ").innerHTML = contentSpan;
+   
+  //duyệt lấy value của array nhanVien
+  for (let thuocTinh in nhanVien) {
+        
+    let tagValue = nhanVien[thuocTinh];
+    
+    console.log(tagValue);
+}
+
+  
 };
