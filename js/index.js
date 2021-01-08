021218be79b2458be3de138c9bde025a3c0cf205
tuanlt79 <63086228+tuanlt79@users.noreply.github.com>
2021-01-08 10:43:55 +0700

    ChangeSpan

--- js/index.js
+++ js/index.js
@@ -1,39 +1,19 @@
 document.getElementById("btnTinhLuong").onclick = (event) => {
     event.preventDefault();
     let nhanVien = {};
-    let arrSpan=[];
     let arrInput = document.querySelectorAll(".form-input input,select ");
-   //duyệt for trên ary input
+    //duyệt for trên ary input
+    let contentSpan = `<span>`;
     for (let i = 0; i < arrInput.length; i++) {
-        
         // mỗi lần duyệt lấy ra 1 tagInput
-        let tagInput = arrInput[i];
+        let tagInput = arrInput[i]; 
         //lấy ra id, value tag input
-        let { id, value,title }= tagInput;
+        let { id, value } = tagInput;
         nhanVien = { ...nhanVien, [id]: value };
-            //tạo span 
-        arrSpan = [...arrSpan, title];
-        console.log(arrSpan);
+        contentSpan += `<p class="text-warning">${tagInput.title} : ${tagInput.value}</p> `; 
     };
-    
     let { selectCV, luongCoBan } = nhanVien;
     let tongLuong = (Number(selectCV) * Number(luongCoBan));
-
-    let contentSpan = ``;
-    for (let i = 0; i < arrSpan.length; i++){
-        contentSpan += `<p class="text-warning">${arrSpan[i]} :</p> `;
-    }
-    contentSpan += `<p class="text-warning">Tổng lương: ${tongLuong}</p>`;
-    
+    contentSpan+=`<p class="text-warning">Tổng Lương: ${tongLuong}</p>`
     document.querySelector(".container .bg-dark form ").innerHTML = contentSpan;
-   
-  //duyệt lấy value của array nhanVien
-  for (let thuocTinh in nhanVien) {
-        
-    let tagValue = nhanVien[thuocTinh];
-    
-    console.log(tagValue);
-}
-
-  
 };
